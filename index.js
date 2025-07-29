3527454930f7e58920e6b50b4c0cc94fc73fdd4d
MAG-analytics <monarchairgroupanalytics@gmail.com>
2025-07-29 15:54:19 -0400

    socket mode

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { App } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  appToken: process.env.SLACK_APP_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true
 });
 
 let queue = ['@Fengyu Xu', '@Ryan DeBruyne', '@Cade Kaftel', '@Michael Loff'];
