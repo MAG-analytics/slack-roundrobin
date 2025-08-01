a470c5e572ed1cbe4abf517ce356fc1bf7202538
MAG-analytics <monarchairgroupanalytics@gmail.com>
2025-08-01 11:33:35 -0400

    add socketmode

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { App } = require('@slack/bolt');
 // Initialize your app
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true
 });
 
 const queues = {
